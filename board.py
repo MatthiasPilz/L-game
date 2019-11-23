80512ed1e6de2f3a4ac2eadc816a61f75b0ec9b1
MatthiasPilz <Matthias.Pilz@kingston.ac.uk>
2019-11-23 11:01:48 +0000

    fixed starting position output

--- board.py
+++ board.py
@@ -1,10 +1,16 @@
-def display_position( pos ):
-	print("")
-	print("   ---------------- ")
-	
-	for i in range ( 4, 0 ):
-		print("%d | %s | %s | %s | %s | ", 
-			%(i, pos[i][0], pos[i][1], pos[i][2], pos[i][3]) )
-		print("  --------------------  ")
-		
-	print("     A    B    C    D   ")
+def display_position(pos):
+    print("   %s to move" %(pos[4][0]))
+    print("                     ")
+    print("   ---------------   ")
+    for i in range(3, -1, -1):
+        line = "" + str(i+1) + " | "
+        for j in range(4):
+            line = line + pos[i][j] + " | "
+
+        print(line)
+        if i > 0:
+            print("  |---------------|  ")
+
+    print("   ---------------   ")
+    print("    A   B   C   D    ")
+    print("                     ")
--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-import board.py
+import board
 
-pos = ["*OO ", " XO ", " XO ", " XX*"]
-display_position(pos)
+# macros
+STARTPOS = [" OO*", " OX ", " OX ", "*XX ", "X"]
+
+
+def main():
+    board.display_position( STARTPOS )
+    
+    
+    
+
+if __name__ == "__main__":
+    main()
+
